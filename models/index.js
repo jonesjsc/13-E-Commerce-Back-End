dc6bb9c65f362bcfa97537a041c19d2b4b1af69c
Jason Jones <jason.e.jones@gmail.com>
2021-04-26 00:30:27 -0400

    tweaking model

--- models/index.js
+++ models/index.js
@@ -7,12 +7,11 @@ const ProductTag = require("./ProductTag");
 // Products belongsTo Category
 Product.belongsTo(Category, {
   foreignKey: "category_id",
-  onDelete: "CASCADE",
 });
 
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: "product_id",
+  foreignKey: "category_id",
 });
 
 // Products belongToMany Tags (through ProductTag)
