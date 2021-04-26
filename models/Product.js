625dfe6449210356e3d96e8ceb3aa139a30609ea
Jason Jones <jason.e.jones@gmail.com>
2021-04-26 00:40:51 -0400

    tweaking the models still

--- models/Product.js
+++ models/Product.js
@@ -38,7 +38,7 @@ Product.init(
       type: DataTypes.INTEGER,
       allowNull: true,
       references: {
-        model: "Category", // Can be both a string representing the table name or a Sequelize model
+        model: "category",
         key: "id",
       },
     },
