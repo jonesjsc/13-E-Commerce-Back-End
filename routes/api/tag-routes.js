cef164a320952e28c770a18a98963f70a6304d5e
Jason Jones <jason.e.jones@gmail.com>
2021-04-26 02:22:04 -0400

    tweaked the routes I think for the last time

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -8,7 +8,7 @@ router.get("/", async (req, res) => {
     // find all tags
     const tagData = await Tag.findAll({
       // be sure to include its associated Product data
-      include: [{ model: Product, through: ProductTag, as: "tags" }],
+      include: [{ model: Product, through: ProductTag }],
     });
     res.status(200).json(tagData);
   } catch (err) {
@@ -21,7 +21,7 @@ router.get("/:id", async (req, res) => {
     // find a single tag by its `id`
     const tagData = await Tag.findByPk(req.params.id, {
       // be sure to include its associated Product data
-      include: [{ model: Product, through: ProductTag, as: "tags" }],
+      include: [{ model: Product, through: ProductTag }],
     });
     if (!tagData) {
       res.status(404).json({ message: `No tag with ID ${req.params.id}` });
